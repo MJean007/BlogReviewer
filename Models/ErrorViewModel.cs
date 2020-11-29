1cf0f7671489b1e4ee178dfa5bc764096c8c47e7
Michel R. Jean <mrjean@videotron.ca>
2020-11-29 07:19:54 -0500

    Added all files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogReviewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

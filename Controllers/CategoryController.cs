2fe71eea33d74399973bdbc58a8e204700ef3588
Michel R. Jean <mrjean@videotron.ca>
2020-11-30 11:56:18 -0500

    It compiles ready to test addition of a category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlogEngine.Models;
+namespace BlogReviewer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public void Add(BlogEngine.Models.category model)
+        {
+            BlogDBContext context = new BlogDBContext();
+            category cat = new category();
+            cat.title = model.title;
+            if (context != null)
+            {
+                context.Categories.Add(cat);
+                context.SaveChanges();
+            }
+            
+        }
+
+    }
+}
